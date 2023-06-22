7c21eec31ce0e55b262a19a3069e163b5fef12b7
gapsmd <l18020781@veracruz.tecnm.mx>
2023-06-22 14:19:28 -0600

    Presentación subdirección

---
+++ src/View/inicio.php
@@ -0,0 +1,381 @@
+<!-- Carrusel de portada -->
+<section>
+  <div id="myCarousel" class="carousel slide col-lg-12 col-sm-12" data-bs-ride="carousel">
+    <div class="carousel-indicators">
+      <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" class="active" aria-current="true"
+        aria-label="Slide 1"></button>
+      <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
+      <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    </div>
+
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="img/ITVER-1.webp" class="d-block w-100 align-middle" width="100%" height="100%">
+        <div class="container">
+          <div class="carousel-caption d-flex flex-column">
+            <h1 class="mainTitle text-center font-bold m-2 shadow-text">Ingeniería Industrial</h1>
+            <h2 class="mainSubtitle text-center font-bold shadow-text">Desarrolla nuevos horizontes</h1>
+              <p><a class="mainButton btn font-bold d-none d-md-inline-flex" href="?option=3">Explorar</a>
+              </p>
+          </div>
+        </div>
+      </div>
+
+      <div class="carousel-item">
+        <img src="img/IMG-38.webp" class="d-block w-100 align-middle" width="100%" height="100%">
+        <div class="container">
+          <div class="carousel-caption">
+            <h1 class="mainTitle text-center font-bold m-2 shadow-text">Ingeniería Industrial</h1>
+            <h2 class="mainSubtitle text-center font-bold shadow-text">Aprende nuevas tecnologías</h1>
+              <p><a class="mainButton btn font-bold d-none d-md-inline-flex" href="?option=1">Explorar</a></p>
+          </div>
+        </div>
+      </div>
+
+      <div class="carousel-item">
+        <img src="img/carrusel-ind-3.webp" class="d-block w-100 align-middle" width="100%" height="100%">
+        <div class="container">
+          <div class="carousel-caption">
+            <h1 class="mainTitle text-center font-bold m-2 shadow-text">Ingeniería Industrial</h1>
+            <h2 class="mainSubtitle text-center font-bold d-none d-md-inline-flex shadow-text">Colabora con la comunidad</h1>
+            <p><a class="mainButton btn font-bold d-none d-md-inline-flex" href="?option=5">Explorar</a></p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <button class="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#myCarousel" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+  </div>
+</section>
+<!--Fin Carrusel de portada -->
+
+<!-- Razones para estudiar la carrera -->
+<section class="p-2">
+  <div class="row px-2 g-0">
+    <div class="col-lg-6 col-12 p-2 shadow-sm">
+      <div class="d-flex justify-content-center align-items-center w-100 h-100">
+        <img class="img-fluid rounded" src="img/ITVER-9.webp" alt="">
+      </div>
+    </div>
+
+    <div class="lightSection col-lg-6 col-12 p-4">
+      <div class="d-flex justify-content-center align-items-center w-100 h-100">
+        <div class="row g-0">
+          <div class="col-12">
+            <h2 class="sectionTitle text-center font-bold m-3">¿Por qué estudiar Ingeniería Industrial?</h2>
+            <div class="sectionSeparator"></div>
+          </div>
+          <div class="col-12" style="text-align: center;">
+            <p style="text-align: justify;"> La Ingeniería Industrial es una de las ramas de la Ingeniería que tiene por objetivo poder brindar crecimiento en las empresas, reduciendo costos. Que, a su vez busca generar más empleos, cuidar el medio ambiente y generar mejores condiciones de trabajo.  
+            Este tipo de Ingeniería es importante puesto que la producción en los sectores de manufactura y de servicio requiere de constante simplificación en sus procesos. Además de que también necesita crear mejoras y soluciones a los problemas de producción. 
+            </p>
+            <a class="btn btn-warning font-bold" href="?option=3" role="button">¡Saber más!</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<!--Fin Razones para estudiar la carrera -->
+
+<!-- Areas de aprendizaje -->
+<section class="lightSection bg-light p-2">
+  <h2 class="sectionTitle text-center font-bold m-3">Áreas de aprendizaje</h2>
+  <div class="sectionSeparator"></div>
+
+  <div class="row g-0">
+
+    <div class="col-lg-4 col-sm-6 text-center p-3">
+      <div class="area shadow-sm p-4">
+        <a href="?option=3.php">
+          <img class="areaImage items-center" src="img/topicos/programacion-icono.PNG" alt="">
+        </a>
+        <h3 class="areaTitle text-center font-bold text-xl">Calidad y productividad</h3>
+        <p class="areaText">Pendiente.</p>
+      </div>
+    </div>
+
+    <div class="col-lg-4 col-sm-6 text-center p-3">
+      <div class="area shadow-sm p-4">
+        <a href="?option=3">
+          <img class="areaImage items-center" src="img/topicos/ing-software-icono.PNG" alt="">
+        </a>
+        <h3 class="areaTitle text-center font-bold text-xl">Control de Procesos</h3>
+        <p class="areaText">Pendiente</p>
+      </div>
+    </div>
+
+    <div class="col-lg-4 col-sm-6 text-center p-3">
+      <div class="area shadow-sm p-4">
+        <a href="?option=3.php">
+          <img class="areaImage items-center" src="img/topicos/admon-bd-icono.PNG" alt="">
+        </a>
+        <h3 class="areaTitle text-center font-bold text-xl">Logística</h3>
+        <p class="areaText">Pendiente</p>
+      </div>
+    </div>
+
+    <div class="col-lg-4 col-sm-6 text-center p-3">
+      <div class="area shadow-sm p-4">
+        <a href="?option=3">
+          <img class="areaImage items-center" src="img/topicos/programacion-web-icono.PNG" alt="">
+        </a>
+        <h3 class="areaTitle text-center font-bold text-xl">Formulación y Evaluación de Proyectos de Inversión</h3>
+        <p class="areaText">Pendiente</p>
+      </div>
+    </div>
+
+    <div class="col-lg-4 col-sm-6 text-center p-3">
+      <div class="area shadow-sm p-4">
+        <a href="?option=3">
+          <img class="areaImage items-center" src="img/topicos/admon-red-icono.PNG" alt="">
+        </a>
+        <h3 class="areaTitle text-center font-bold text-xl">Planeación y Control de la Productividad</h3>
+        <p class="areaText">Pendiente</p>
+      </div>
+    </div>
+
+    <div class="col-lg-4 col-sm-6 text-center p-3">
+      <div class="area shadow-sm p-4">
+        <a href="?option=3">
+          <img class="areaImage items-center" src="img/topicos/inteligencia-artificial-icono.PNG" alt="">
+        </a>
+        <h3 class="areaTitle text-center font-bold text-xl">Seguridad e Higiene Industrial</h3>
+        <p class="areaText">Pendiente</p>
+      </div>
+    </div>
+
+  </div>
+</section>
+<!-- Fin Áreas de aprendizaje-->
+
+
+<!-- Instalaciones -->
+<section class="darkSection bg-dark p-2 px-4">
+  <div class="row mb-4 px-2 g-0">
+
+    <div class="col-lg-6 col-12 py-4">
+      <div class="d-flex justify-content-center align-items-center w-100 h-100">
+        <div class="row g-0">
+          <div class="col-12">
+            <h2 class="titleDarkSection text-center font-bold m-3">Instalaciones</h2>
+            <div class="darkSectionSeparator"></div>
+          </div>
+          <div class="col-12 px-4" style="text-align: center;">
+            <p class="text-light" style="text-align: justify;">
+              El departamento de Ingeniería Industrial cuenta con aulas diseñadas adecuadamente para el desempeño de la actividad educativa; sala de antropometría, sala de seguridad e higiene, sala digital y laboratorio de estudio de trabajo.
+              <br></br>
+              Las instalaciones están orientadas para el desarrollo de las competencias necesarias de los estudiantes asi como para llevar a cabo un aprendizaje didáctico, desarrollando proyectos y aplicando herramientas de Ingeniería Industrial.
+            </p>
+            <a class="btn btn-warning font-bold" href="?option=4" role="button">¡Explorar!</a>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-lg-6 col-12 p-4 shadow-sm">
+      <div class="d-flex justify-content-center align-items-center w-100 h-100">
+        <img class="img-fluid rounded" src="img/IMG-27.webp" alt="">
+      </div>
+    </div>
+
+  </div>
+</section>
+<!--Fin Instalaciones-->
+
+<!-- Comunidad docente  -->
+<section class="p-2 bg-light">
+  <div class="row px-2 g-0">
+
+    <div class="col-lg-6 col-sm-12">
+      <div class="galleryContainer bg-light">
+        <div class="row g-0">
+          <div class="col-4 px-2">
+            <img class="img-fluid w-100 shadow-1-strong rounded m-2" src="img/Docentes/brendaCastroPerez.webp"
+              alt="">
+            <img class="img-fluid w-100 shadow-1-strong rounded m-2"
+              src="img/Docentes/enriqueAntonioRochaAltamira.webp" alt="">
+          </div>
+
+          <div class="col-4 px-2">
+            <img class="img-fluid w-100 shadow-1-strong rounded m-2" src="img/Docentes/oscarPerezArellano.webp"
+              alt="">
+            <img class="img-fluid w-100 shadow-1-strong rounded m-2" src="img/Docentes/karinaGomezCantarell.webp" alt="">
+          </div>
+
+          <div class="col-4 px-2">
+            <img class="img-fluid w-100 shadow-1-strong rounded m-2" src="img/Docentes/deliadelCarmenGamboaOlivares.webp"
+              alt="">
+            <img class="img-fluid w-100 shadow-1-strong rounded m-2" src="img/Docentes/antonioChaparroVadillo.webp"
+              alt="">
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+    <div class="lightSection col-lg-6 col-12 px-4">
+      <div class="d-flex justify-content-center align-items-center w-100 h-100">
+        <div class="row g-0">
+          <div class="col-12">
+            <h2 class="sectionTitle text-center font-bold m-3">Comunidad docente</h2>
+            <div class="sectionSeparator"></div>
+          </div>
+          <div class="col-12" style="text-align: center;">
+            <p style="text-align: justify;"> Nuestros profesores son impulsores de cambio, son el elemento esencial de
+              nuestra institución, contamos con profesores y profesoras de diversas disciplinas, quienes demuestran diariamente 
+              su compromiso más allá de las aulas. Son profesionales que tienen la facultad de transformar vidas, de impactar a 
+              las futuras generaciones de estudiantes.
+            </p>
+            <a class="btn btn-warning font-bold" href="?option=2" role="button">¡Conocer docentes!</a>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<!-- Fin Comunidad docente -->
+
+<!-- Enlaces -->
+<section class="lightSection bg-light p-2">
+  <h2 class="sectionTitle text-center font-bold m-3">Enlaces</h2>
+  <div class="sectionSeparator"></div>
+
+  <div class="row g-0">
+
+    <div class="col-lg-4 col-sm-6 text-center p-3">
+      <div class="area shadow-sm p-4 rounded-3">
+        <a href="http://gestion.itver.edu.mx/servicio-social/" target="_blank" >
+          <img class="img-fluid w-100 shadow-1-strong rounded mb-4 p-1" src="img/IMG_1223.webp" alt="">
+        </a>
+        <h3 class="areaTitle text-center font-bold text-xl">Servicio social</h3>
+      </div>
+    </div>
+
+    <div class="col-lg-4 col-sm-6 text-center p-3">
+      <div class="area shadow-sm p-4 rounded-3">
+        <a href="http://www.veracruz.tecnm.mx/" target="_blank" >
+          <img class="img-fluid w-100 shadow-1-strong rounded mb-4  p-1" src="img/ITVER-9.webp" alt="">
+        </a>
+        <h3 class="areaTitle text-center font-bold text-xl">TECNM Veracruz</h3>
+      </div>
+    </div>
+
+    <div class="col-lg-4 col-sm-6 text-center p-3">
+      <div class="area shadow-sm p-4 rounded-3">
+        <a href="?option=5">
+          <img class="img-fluid w-90 shadow-1-strong rounded mb-4  p-1" src="img/comunidad.webp" alt="">
+        </a>
+        <h3 class="areaTitle text-center font-bold text-xl">Comunidades</h3>
+      </div>
+    </div>
+
+    
+
+  </div>
+</section>
+<!-- Fin Enlaces ->
+
+<!-- Área de redes sociales -->
+<section class="lightSection p-2 g-0">
+  <div class="row g-0">
+    <div class="col">
+      <h2 class="sectionTitle text-center font-bold m-3">Redes sociales</h2>
+      <div class="sectionSeparator"></div>
+    </div>
+  </div>
+</section>
+<section class="p-2 g-0">
+  <div class="row g-0">
+    <div class="col-12 col-md-6">
+
+      <h4 class="sectionTitle text-center font-bold">Tecnológico Nacional de México</h4>
+
+      <div class="d-flex flex-row justify-content-center">
+        <a class="mx-2" href="https://www.tecnm.mx/" target="_blank" style="text-decoration: none;">
+          <div class="d-flex flex-column justify-content-center">
+            <img class="img-fluid shadow-1-strong rounded mb-4 p-1" style="height: 40px"
+              src="img/iconos/tecnm-logo.PNG" alt="">
+          </div>
+        </a>
+        <a class="mx-2" href="https://www.facebook.com/TecNMmx" target="_blank" style="text-decoration: none;">
+          <div class="d-flex flex-row justify-content-center">
+            <img class="img-fluid shadow-1-strong rounded mb-4 p-1" style="height: 40px" src="img/iconos/facebook.PNG"
+              alt="">
+          </div>
+        </a>
+        <a class="mx-2" href="https://twitter.com/TecNM_MX" target="_blank" style="text-decoration: none;">
+          <div class="d-flex flex-row justify-content-center">
+            <img class="img-fluid shadow-1-strong rounded mb-4 p-1" style="height: 40px" src="img/iconos/twitter.PNG"
+              alt="">
+          </div>
+        </a>
+        <a class="mx-2" href="https://www.youtube.com/user/SNESTMX" target="_blank" style="text-decoration: none;">
+          <div class="d-flex flex-row justify-content-center">
+            <img class="img-fluid shadow-1-strong rounded mb-4 p-1" style="height: 40px" src="img/iconos/youtube.PNG"
+              alt="">
+          </div>
+        </a>
+      </div>
+    </div>
+    <div class="col-12 col-md-6">
+      <h4 class="sectionTitle  text-center font-bold">Instituto Tecnológico de Veracruz</h4>
+      <div class="d-flex flex-row justify-content-center">
+        <a class="mx-2" href="https://www.veracruz.tecnm.mx/index.php" target="_blank" style="text-decoration: none;">
+          <div class="d-flex flex-row justify-content-center">
+            <img class="img-fluid shadow-1-strong rounded mb-4 p-1" style="height: 40px"
+              src="img/iconos/itver-logo.PNG" alt="">
+          </div>
+        </a>
+        <a class="mx-2" href="https://www.facebook.com/ITVeracruz" target="_blank" style="text-decoration: none;">
+          <div class="d-flex flex-row justify-content-center">
+            <img class="img-fluid shadow-1-strong rounded mb-4 p-1" style="height: 40px" src="img/iconos/facebook.PNG"
+              alt="">
+          </div>
+        </a>
+        <a class="mx-2" href="https://twitter.com/tvitver" target="_blank" style="text-decoration: none;">
+          <div class="d-flex flex-row justify-content-center">
+            <img class="img-fluid shadow-1-strong rounded mb-4 p-1" style="height: 40px" src="img/iconos/twitter.PNG"
+              alt="">
+          </div>
+        </a>
+        <a class="mx-2" href="https://www.youtube.com/channel/UCVBkOP_rj5ulvlriG88jTzA" target="_blank"
+          style="text-decoration: none;">
+          <div class="d-flex flex-row justify-content-center">
+            <img class="img-fluid shadow-1-strong rounded mb-4 p-1" style="height: 40px" src="img/iconos/youtube.PNG"
+              alt="">
+          </div>
+        </a>
+      </div>
+    </div>
+  </div>
+  <div class="row g-0">
+    <h4 class="sectionTitle  text-center font-bold">Coordinación Industrial</h4>
+    <div class="d-flex flex-row justify-content-center">
+      <a class="mx-2" href="https://www.facebook.com/CoordinacionIndustrialITVer/" target="_blank"
+        style="text-decoration: none;">
+        <div class="d-flex flex-row justify-content-center">
+          <img class="img-fluid shadow-1-strong rounded mb-4 p-1" style="width: 40px" src="img/iconos/facebook.PNG"
+            alt="">
+        </div>
+      </a>
+      <!--<a class="mx-2" href="https://www.youtube.com/channel/UCYSUOZ6V9YRGviVvwaXOcjw" target="_blank"
+          style="text-decoration: none;">
+          <div class="d-flex flex-row justify-content-center">
+            <img class="img-fluid shadow-1-strong rounded mb-4 p-1" style="height: 40px" src="img/iconos/youtube.PNG"
+              alt="">
+          </div>
+        </a>
+-->
+    </div>
+  </div>
+</section>
+<!-- Fin Área de redes sociales -->
